a85a9338fbf4c25ecf4c250c68c2fe58cdf46fde
thunlt <thuntl.vnit@gmail.com>
2023-06-05 22:24:25 +0700

    commit toi slide 2 luyen dich 3600 mat hang tang gia

--- SourceVideo/js/main.js
+++ SourceVideo/js/main.js
@@ -5,13 +5,13 @@ function toggleParagraph(index) {
   if (paragraph.classList.contains("active")) {
     paragraph.classList.remove("active");
     paragraph.style.transform = "none";
-    paragraph.style.color = "black";
+    paragraph.classList.remove("highlight");
     audio.pause();
     audio.currentTime = 0;
   } else {
     paragraph.classList.add("active");
     paragraph.style.transform = "translateY(-5px)";
-    paragraph.style.color = "red";
+    paragraph.classList.add("highlight");
     audio.play();
   }
 }
